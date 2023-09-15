413f2a6fdc3dca886b9d321944d11139094f5cb4
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-15 11:40:13 -0300

    Initial Commit

---
+++ SwiftUIManzi/SwiftUIManziApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIManziApp.swift
+//  SwiftUIManzi
+//
+//  Created by Christian Manzaraz on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIManziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
